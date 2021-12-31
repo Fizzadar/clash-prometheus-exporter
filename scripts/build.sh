937d0ed5d207c9d96da235817e547165fdb68ea0
Nick Barrett <nick@fizzadar.com>
2021-12-31 16:06:25 +0000

    Update build script to work with git tags.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -exuo pipefail
+
+tag=$(git tag --points-at HEAD | cut -d v -f 2)
+
 docker buildx build \
     --no-cache \
     --push \
     --platform linux/amd64,linux/arm64 \
-    -t fizzadar/clash-prometheus-exporter:0.2 \
+    -t fizzadar/clash-prometheus-exporter:$tag \
     .
